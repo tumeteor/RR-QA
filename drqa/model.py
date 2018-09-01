aaaa947dbd063c1ce13f8681a5e51b15e764fe2f
tumeteor <tumeteor@gmail.com>
2018-09-01 13:40:57 +0100

    modified:   drqa/model.py

--- drqa/model.py
+++ drqa/model.py
@@ -147,18 +147,20 @@ class DocReaderModel(object):
             cand_size = len(ex[0])
             inputs = []
             for i in range(0, cand_size):
-                inputs.append([Variable(e.cuda(async=True)).unsqueeze(0) for e in (ex[0][i], ex[1][i],
+                x = [Variable(e.cuda(async=True)).unsqueeze(0) for e in (ex[0][i], ex[1][i],
                                                                       ex[2][i], ex[3][i],
-                                                                      ex[4][i])].
+                                                                      ex[4][i])]
+                inputs.append(x.
                               extend([Variable(e.cuda(async=True))for e in (ex[5], ex[6])]))
 
         else:
             cand_size = len(ex[0])
             inputs = []
             for i in range(0, cand_size):
-                inputs.append([Variable(e).unsqueeze(0) for e in (ex[0][i], ex[1][i],
+                x = [Variable(e).unsqueeze(0) for e in (ex[0][i], ex[1][i],
                                                                       ex[2][i], ex[3][i],
-                                                                      ex[4][i])].
+                                                                      ex[4][i])]
+                inputs.append(x.
                               extend([Variable(e.cuda(async=True))for e in (ex[5], ex[6])]))
 
 
